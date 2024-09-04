16d23f7723084fc90b292a6f65405a3853ed8b84
fxf981 <105899270+fxf981@users.noreply.github.com>
2024-09-04 11:13:14 +0800

    Update core.go

--- core/core.go
+++ core/core.go
@@ -39,7 +39,7 @@ func Version() string {
 // VersionStatement returns a list of strings representing the full version info.
 func VersionStatement() []string {
 	return []string{
-		serial.Concat("node.js ", Version(), " (", codename, ") ", build, " (", runtime.Version(), " ", runtime.GOOS, "/", runtime.GOARCH, ")"),
+		serial.Concat("no.js ", Version(), " (", codename, ") ", build, " (", runtime.Version(), " ", runtime.GOOS, "/", runtime.GOARCH, ")"),
 		intro,
 	}
 }
